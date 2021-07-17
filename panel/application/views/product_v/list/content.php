aff7725eac6be533f711c54d8458415fc9bd000b
bekirtolaman <tolamanbekir@gmail.com>
2021-07-17 15:02:50 +0300

    Butonlar eklendi. Alert paneli eklendi.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,8 +1,20 @@
 <div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">
+            Ürün Listesi
+            <a href="#" class="btn btn-outline btn-primary btn-xs pull-right"><i class="fa fa-plus"></i>Yeni Ekle</a>
+        </h4>
+    </div>
+
+    
 
 	<div class="col-md-12">
 				<div class="widget p-lg">
 
+                <div class="alert alert-info text-center">
+		                <h5 class="alert-title">Kayıt Bulunumadı.</h5>
+		                <p>Herhangi bir veri bulunamadı. Eklemek için lütfen <a href="#"> tıklayınız.</a></p>
+	            </div>
 					<table class="table table-hover table-striped">
 						<thead>
                             <th>#id</th>
@@ -19,7 +31,34 @@
                                 <td>deneme-deneme</td>
                                 <td>deneme</td>
                                 <td>deneniyor</td>
-                                <td>1/0</td>
+                                <td>
+								<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+							</td>
+                                <td>
+                                <a type="button" class="btn btn-xs btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+                                <a type="button" class="btn btn-xs btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>#1</td>
+                                <td>deneme-deneme</td>
+                                <td>deneme</td>
+                                <td>deneniyor</td>
+                                <td>
+								<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+							</td>
+                                <td>
+                                <a type="button" class="btn btn-xs btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+                                <a type="button" class="btn btn-xs btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+                                </td>
+                            </tr>             <tr>
+                                <td>#1</td>
+                                <td>deneme-deneme</td>
+                                <td>deneme</td>
+                                <td>deneniyor</td>
+                                <td>
+								<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+							</td>
                                 <td>
                                 <a type="button" class="btn btn-xs btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
                                 <a type="button" class="btn btn-xs btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
