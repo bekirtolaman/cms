656acb77d95ed2db88e544aba9e2a110c58ab693
bekirtolaman <tolamanbekir@gmail.com>
2021-07-17 11:37:30 +0300

    Ürünler modülü için tablo oluşturuldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,33 @@
-ürün listesi
+<div class="row">
+
+	<div class="col-md-12">
+				<div class="widget p-lg">
+
+					<table class="table table-hover table-striped">
+						<thead>
+                            <th>#id</th>
+                            <th>url</th>
+                            <th>Başlık</th>
+                            <th>Açıklama</th>
+                            <th>Durumu</th>
+                            <th>İşlem</th>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>#1</td>
+                                <td>deneme-deneme</td>
+                                <td>deneme</td>
+                                <td>deneniyor</td>
+                                <td>1/0</td>
+                                <td>
+                                <a type="button" class="btn btn-xs btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+                                <a type="button" class="btn btn-xs btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+                                </td>
+                            </tr>
+                        </tbody>
+					</table>
+				</div><!-- .widget -->
+			</div><!-- END column -->
+
+</div>
